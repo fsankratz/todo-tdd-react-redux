e161ad822a6cc241e3f1aa4b45c2609a09ee9d6b
Fabian Sander Kratz <30731536+fsankratz@users.noreply.github.com>
2022-03-15 14:50:06 +0100

    tdd todo app!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useCallback, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([])
+    const [todoInput, setTodoInput] = useState('')
+    const handleAddTodo = useCallback(() => {
+        setTodos([...todos, {text: todoInput, complete: false, id: Math.round(Math.random() * 100000)}])
+        setTodoInput('')
+    }, [setTodos, todos, todoInput, setTodoInput])
+    const toggleComplete = useCallback(id => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, complete: !todo.complete} : todo))
+    }, [setTodos, todos])
+    const removeTodo = useCallback(id => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }, [setTodos, todos])
+    const clearTasks = useCallback(() => {
+        setTodos([])
+    }, [setTodos])
+    return (
+        <div className={'flex flex-col items-center pt-72 pb-32 w-full font-mono'}>
+            <div className={'w-96 flex flex-col py-6 px-10 bg-slate-100 rounded-xl'}>
+                <label htmlFor={'todo'}
+                    className={'text-xl mb-2'}
+                >Todo:</label>
+                <form className={'flex mb-8'} onSubmit={e => e.preventDefault()}>
+                    <input
+                        className={'px-3 py-2 grow mr-3 rounded-lg border border-gray-300 shadow-sm'}
+                        maxLength={20}
+                        type={'text'} id={'todo'} onChange={e => setTodoInput(e.target.value)} value={todoInput}/>
+                    <button
+                        disabled={todoInput === ''}
+                        className={`${todoInput === '' ? 'bg-emerald-200' : ''} px-3 py-2 rounded-lg bg-emerald-600 text-white shadow-sm`}
+                        onClick={handleAddTodo}>Add</button>
+                </form>
+                {todos.length === 0 && <p>Nothing here yet...</p>}
+                {todos.length !== 0 && (
+                    <>
+                        <ul className={'mb-4'}>
+                            {todos.map(todo => (
+                                <li key={todo.id} className={'flex bg-white px-3 py-2 rounded-md mb-3 shadow-sm'}>
+                                    <button
+                                        tabIndex={0}
+                                        className={`${todo.complete ? 'line-through' : ''} grow select-none`}
+                                        onClick={() => toggleComplete(todo.id)}
+                                    >{todo.text}</button>
+                                    <button
+                                        className={'underline text-gray-400'}
+                                        onClick={() => removeTodo(todo.id)}>Remove</button>
+                                </li>
+                            ))}
+                        </ul>
+                        <button onClick={clearTasks} className={'text-sm text-red-500'}>Clear all</button>
+                    </>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/__tests__/main.js
@@ -0,0 +1,118 @@
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+const renderAppWith = (...args) => {
+    render(<App />)
+    args.forEach(arg => arg())
+}
+
+describe('after loading the app', () => {
+    const washDishes = "wash dishes"
+    const enterWashDishes = () => {
+        const todoInput = screen.getByLabelText('Todo:')
+        userEvent.type(todoInput, washDishes)
+    }
+    test('when there are no todos, see text that says "nothing here yet"', () => {
+        renderAppWith()
+        const nothingHereText = screen.getByText("Nothing here yet...")
+        expect(nothingHereText).toBeInTheDocument()
+    })
+
+    test('see an input field to add an item', () => {
+        renderAppWith()
+        const todoInput = screen.getByLabelText('Todo:')
+        expect(todoInput).toBeInTheDocument()
+    })
+
+    test('see a button saying "add"', () => {
+        renderAppWith()
+        const addBtn = screen.getByRole('button', { name: 'Add'})
+        expect(addBtn).toBeInTheDocument()
+    })
+    test('the add button is disabled when input is empty', () => {
+        renderAppWith()
+        const addBtn = screen.getByRole('button', { name: 'Add'})
+        expect(addBtn).toBeDisabled()
+    })
+
+    describe('after adding a todo item', () => {
+        const addWashDishesTodo = () => {
+            enterWashDishes()
+            const addBtn = screen.getByRole('button', { name: 'Add'})
+            userEvent.click(addBtn)
+        }
+        test('hide the text saying "nothing here yet"', () => {
+            renderAppWith(addWashDishesTodo)
+            const nothingHereText = screen.queryByText('Nothing here yet...')
+            expect(nothingHereText).not.toBeInTheDocument()
+        })
+        test('see the text I entered in the todo list', () => {
+            renderAppWith(addWashDishesTodo)
+            const todo = screen.getByText(washDishes)
+            expect(todo).toBeInTheDocument()
+        })
+        test('clear the input', () => {
+            renderAppWith(addWashDishesTodo)
+            const todoInput = screen.getByLabelText('Todo:')
+            expect(todoInput).toHaveDisplayValue('')
+        })
+        test('show a button to remove the todo', () => {
+            renderAppWith(addWashDishesTodo)
+            const removeBtn = screen.getByRole('button', {name: 'Remove'})
+            expect(removeBtn).toBeInTheDocument()
+        })
+        test('remove the task when clicking on the remove button', () => {
+            renderAppWith(addWashDishesTodo)
+            const removeBtn = screen.getByRole('button', {name:'Remove'})
+            userEvent.click(removeBtn)
+            const todo = screen.queryByText(washDishes)
+            expect(todo).not.toBeInTheDocument()
+        })
+        test('see a button to clear all tasks', () => {
+            renderAppWith(addWashDishesTodo)
+            const clearBtn = screen.getByRole('button', { name: 'Clear all'})
+            expect(clearBtn).toBeInTheDocument()
+        })
+        describe('when clicking on the todo', () => {
+            const clickOnTodoOnce = () => {
+                const todo = screen.getByRole('button', {name: washDishes})
+                userEvent.click(todo)
+            }
+            test('make it strikethrough', () => {
+                renderAppWith(addWashDishesTodo, clickOnTodoOnce)
+                const todo = screen.getByRole('button', {name: washDishes})
+                expect(todo).toHaveClass('line-through')
+            })
+            test('twice, remove strikethrough', () => {
+                renderAppWith(addWashDishesTodo, clickOnTodoOnce, clickOnTodoOnce)
+                const todo = screen.getByRole('button', {name: washDishes})
+                expect(todo).not.toHaveClass('line-through')
+            })
+        })
+        describe('after adding a second item', () => {
+            const doHomework = 'do homework'
+            const addDoHomeworkTodo = () => {
+                const addBtn = screen.getByRole('button', { name: 'Add'})
+                const todoInput = screen.getByLabelText('Todo:')
+                userEvent.type(todoInput, doHomework)
+                userEvent.click(addBtn)
+            }
+            test('see both', () => {
+                renderAppWith(addWashDishesTodo, addDoHomeworkTodo)
+                const washDishesTodo = screen.getByText(washDishes)
+                const doHomeworkTodo = screen.getByText(doHomework)
+                expect(washDishesTodo).toBeInTheDocument()
+                expect(doHomeworkTodo).toBeInTheDocument()
+            })
+            test('after clicking clear all button, see empty list', () => {
+                renderAppWith(addWashDishesTodo, addDoHomeworkTodo)
+                const clearBtn = screen.getByRole('button', { name: 'Clear all'})
+                userEvent.click(clearBtn)
+                const nothingHereText = screen.getByText('Nothing here yet...')
+                expect(nothingHereText).toBeInTheDocument()
+            })
+        })
+    })
+})
